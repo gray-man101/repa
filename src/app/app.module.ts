d4e2b64c2103d67c297f7f218bfa1ff127e2c37f
Aleksej Voroncov <a.voroncovs@bill.me>
2019-09-03 13:32:24 +0300

    responsive grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,15 @@ import {AppMaterialModule} from './app.material.module';
 import {AppComponent, DialogContentComponent} from './app.component';
 import {RouterModule} from '@angular/router';
 
+import {MatGridListResponsiveModule} from '../lib/mat-grid-list-responsive/mat-grid-list-responsive.module';
+
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     AppMaterialModule,
     BrowserAnimationsModule,
+    MatGridListResponsiveModule,
     RouterModule.forRoot([])
   ],
   declarations: [AppComponent, DialogContentComponent],
